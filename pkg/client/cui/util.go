39cec39e40e9d63b3147b5b53b5804b4264317e7
wkd3475 <wkd3475@gmail.com>
2020-02-05 12:11:38 +0900

    fix : package 이름을 main으로 변경

---
+++ pkg/client/cui/util.go
@@ -0,0 +1,10 @@
+package cui
+
+import "github.com/jroimartin/gocui"
+
+func setCurrentViewOnTop(g *gocui.Gui, name string) (*gocui.View, error) {
+	if _, err := g.SetCurrentView(name); err != nil {
+		return nil, err
+	}
+	return g.SetViewOnTop(name)
+}
